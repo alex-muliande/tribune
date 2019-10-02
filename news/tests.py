e210209f348e46459d5bf4ef5a6ff362d0457e5d
alex-muliande <amuliande@gmail.com>
2019-10-02 12:01:13 +0300

    add date time

--- news/tests.py
+++ news/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Editor,Article,tags
+import datetime as dt
 # Create your tests here.
 
 class EditorTestClass(TestCase):
@@ -16,3 +17,34 @@ class EditorTestClass(TestCase):
         self.alex.save_editor()
         editors = Editor.objects.all()
         self.assertTrue(len(editors) > 0)
+    
+class ArticleTestClass(TestCase):
+    
+    def setUp(self):
+        #Creating a new editor and saving it
+        self.alex= Editor(first_name ='Alex', last_name ='Muliande', email ='alexnad425@gmail.com')
+        self.alex.save_editor()
+
+        #Creating a new tag and saving it 
+        self.new_tag = tags(name ='testing')
+        self.new_tag.save()
+
+        self.new_article = Article(title = 'Test Article',post = 'This is a random test post',editor = self.alex)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+        def tearDown(self):
+            Editor.objects.all().delete()
+            tags.objects.all()
+            Article.objects.all().delete()
+        
+        def test_get_news_today(self):
+            today_news = Article.today_news()
+            self.assertTrue(len(today_news) > 0)
+
+        def test_get_news_date(self):
+            test_date = '2017-03-17'
+            date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+            news_by_date = Article.days_news(date)
+            self.assertTrue(len(news_by_date) == 0)
