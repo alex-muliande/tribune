8103051b7b2a7ed1515273a1c42396133f6fd698
alex-muliande <amuliande@gmail.com>
2019-10-02 11:52:38 +0300

    Add phoone number  charachter field to class Editor

--- news/models.py
+++ news/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 import datetime as db
+import datetime as dt 
 # Create your models here.
 class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
 
     def __str__(self):
         return self.first_name
@@ -25,3 +27,15 @@ class Article(models.Model):
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True, blank = True)
+
+    @classmethod 
+    def todays_news(cls):
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date_date = today)
+        return news
+    
+    @classmethod
+    def days_news(cls,date):
+        news = cls.objects.filter(pub_date_date =date)
+
+        return news
