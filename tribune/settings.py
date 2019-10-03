49fdc26b39c484c17062afc3ecdb00f9e7291b87
alex-muliande <amuliande@gmail.com>
2019-10-03 08:13:02 +0300

    heroku deployment2

--- tribune/settings.py
+++ tribune/settings.py
@@ -23,11 +23,11 @@ if config('MODE')=="dev":
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('tribune'),
-           'USER': config('alex'),
-           'PASSWORD': config('muliande'),
-           'HOST': config('127.0.0.1'),
-           'PORT': '8000',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
        }
        
    }
